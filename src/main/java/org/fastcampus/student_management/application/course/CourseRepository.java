5bcb40dea4225ed798f994c10cada5f7fdfce809
Jjuhui313 <hyacinth92928@gmail.com>
2025-09-13 16:43:26 +0900

    Refactor: interface 추가

---
+++ src/main/java/org/fastcampus/student_management/application/course/CourseRepository.java
@@ -0,0 +1,15 @@
+package org.fastcampus.student_management.application.course;
+
+import java.util.List;
+import org.fastcampus.student_management.domain.Course;
+import org.fastcampus.student_management.domain.DayOfWeek;
+
+public interface CourseRepository {
+
+    public void save(Course course);
+
+    List<Course> getCourseDayOfWeek(DayOfWeek dayOfWeek);
+
+    List<Course> getCourseListByStudent(String studentName);
+
+}
---
+++ src/main/java/org/fastcampus/student_management/repo/CourseJdbcRepository.java
@@ -0,0 +1,24 @@
+package org.fastcampus.student_management.repo;
+
+import java.util.List;
+import org.fastcampus.student_management.application.course.CourseRepository;
+import org.fastcampus.student_management.domain.Course;
+import org.fastcampus.student_management.domain.DayOfWeek;
+
+public class CourseJdbcRepository implements CourseRepository {
+
+    @Override
+    public void save(Course course) {
+
+    }
+
+    @Override
+    public List<Course> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
+        return null;
+    }
+
+    @Override
+    public List<Course> getCourseListByStudent(String studentName) {
+        return null;
+    }
+}
